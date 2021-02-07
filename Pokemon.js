b6e39359260a5ae3a4c5bd17585f48835e55c286
Amarachukwu Ugoanusi <amarachukwuugoanusi@AmarachukwusAir.fios-router.home>
2021-02-07 18:51:23 -0500

    used axios to make API request

--- Pokemon.js
+++ Pokemon.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const pokemon = express();
+const axios = require('axios');
+// const axios = require('axios');
 
 pokemon.set('view engine', 'pug');
 
-pokemon.get('/', (req, res) => {
-    
-    res.render('pokemon');//rendering pug
-});
+// const ajaxRoute = require('./routes/Ajax');
 
+// pokemon.use(ajaxRoute);
+
+pokemon.get('/', async (req, res) => {
+    let pokedexResponse = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
+    console.log(pokedexResponse);
+    res.render('pokemon');
+})
+
+// pokemon.get('/', (req, res) => {
+    
+//     res.render('pokemon');//rendering pug
+// });
+pokemon.use('/static', express.static('public'));
+
+// app.get('/dog/random', async (req, res) => {
+//     let dogResponse = await axios.get("https://dog.ceo/api/breeds/image/random");
+//     res.send(dogResponse.data.message);
+//     console.log(dogResponse.data.message);
+//   })
+  
+  
 pokemon.listen(3000, () => {
     console.log('The application is running on localHost:3000!');
 });
