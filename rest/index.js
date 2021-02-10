dc2640ed2de96cec11f7630591ed3e6cbe883cf6
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-02-09 20:09:30 -0500

    object mapping

--- rest/index.js
+++ rest/index.js
@@ -11,22 +11,18 @@ app.get("/Pokemon", async (req, res) => {
     try{
       const pokeResponse = await fetch("https://pokeapi.co/api/v2/pokemon/1");
       data = await pokeResponse.json();
-      console.log(data);
-      data.types.map(type => type.type.name).join(", ");
-      console.log(data.sprites["front_default"]);
-      // "other": {
-      //   "dream_world": {
-      //       "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
-      //       "front_female": null
-      //   },
-      //   "official-artwork": {
-      //       "front_default"
-      //res.send(data.results[0].name);
-      //res.send(data.results[0].url);
+      console.log(data);  
     } catch (e){
         console.log(e);
     }  
     });
+
+    const pokemon = data.map(data => ({
+      name: data.name,
+      id: data.id,
+      image: data.sprites["front_default"],
+      type: data.types.map(type => type.type.name).join(", "), //????
+    }));
 
     app.listen(3000, () => {
       console.log("Started on Port 3000");
