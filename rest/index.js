8ea0e4308f011b1e09627f7a13e1e64d8f85885e
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-02-11 14:02:24 -0500

    trying to get more than 1 pokemon

--- rest/index.js
+++ rest/index.js
@@ -1,29 +1,38 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const bodyParser = require("body-parser");
+const { removeAllListeners } = require("nodemon");
+let pokemon = [];
 let data;
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/Pokemon", async (req, res) => {
-    try{
-      const pokeResponse = await fetch("https://pokeapi.co/api/v2/pokemon/1");
-      data = await pokeResponse.json();
-      console.log(data);  
-    } catch (e){
-        console.log(e);
-    }  
-    });
+      pokemon = fetch("https://pokeapi.co/api/v2/pokemon/?limit=2")
+      data = await res.json();
+      const pokemon = [];
 
-    const pokemon = data.map(data => ({
-      name: data.name,
-      id: data.id,
-      image: data.sprites["front_default"],
+let allPokemon = data.map((data) => ({
+  name: data.name,
+  id: data.id,
+  image: data.sprites["front_default"]
+}));
+});
+
+
+
+
       
-    }));
+
+     
+
+
+
+      
 
     app.listen(3000, () => {
       console.log("Started on Port 3000");
-});
+}); //end of app.listen
