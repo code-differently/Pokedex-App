3c18d6fdfe85f0ad54ccafe1f07903ff404ee94e
Kim Lai <kimlai@Kims-MacBook-Air.local>
2021-02-12 11:05:00 -0500

    add fixed data to route

--- rest/index.js
+++ rest/index.js
@@ -6,19 +6,27 @@ const app = express()
 app.use(cors())
 const port = 4000
 
-app.get('/all', async (req, response) => {
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=5&offset=0")
+
+
+app.get('/all/:count', async (req, response) => {
+  const count = req.params.count;
+  fetch(`https://pokeapi.co/api/v2/pokemon?limit=${count}&offset=0`)
     .then((res) => res.json())
     .then((allThePokemon) => {
-      const requestsToMake = allThePokemon.results.map(({ url }) =>
-        fetch(url).then((res) => res.json())
-      );
-    return Promise.all(requestsToMake);
-    })
+        const requestsToMake = allThePokemon.results.map(({ url }) =>
+          fetch(url).then((res) => res.json())
+        );
+        return Promise.all(requestsToMake);
+      })
     .then(data => {
-      response.json(data)
+      let responseResult = [];
+      for( let i = 0; i < count; i++){
+        let responseObject = { id: data[i].id, name: data[i].name, type: data[i].types[0].type.name, image: data[i].sprites.back_default};
+        responseResult.push(responseObject);
+      }
+      response.json(responseResult);
     })
-    .catch(err => console.error(err));
+  .catch(err => console.error(err));
 })
 
 
