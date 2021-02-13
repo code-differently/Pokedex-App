2bd8b8c4b9308adb289ac7e535214791b98e5030
Lanay Harris <lanayharris@Lanays-MacBook-Air.local>
2021-02-13 09:56:07 -0500

    creating pull request

--- web/index.js
+++ web/index.js
@@ -24,10 +24,49 @@ button.addEventListener("click", () => {
 
  //function to create cards
 function showPokemon ( id, name, type, image){
+  let pokeName = document.createElement('h4');
+ pokeName.textContent = name;
+
+ let pokeId = document.createElement('h4');
+ pokeId.textContent = id;
+
+ let pokeType = document.createElement('h4');
+ pokeType.textContent = type;
+
+ let pokeImage = document.createElement('sprites');
+ pokeImage.textContent = image;
+ 
+  return(
+    <div class="container" id="cardDeck">
+          <div class="card">
+              <div class="container">
+                <div class="row">
+                    <h4 class="pName">Pikachu</h4> 
+                    <h4 class="pNum">#025</h4>
+                    <div class="line"></div>
+                </div>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"></img>
+              </div>
+          </div>
+      </div>
+  );
    //create a div for card follow html format
  //append to mainContainer.appendChild(variable);
  console.log(name, id, image, type);
- let pokeName = document.createElement('h4');
+
+ /*let div = document.createElement('div');
+ div.innerHTML = '<h4></h4>', '<p></p>', 
+
+ let h4 = document.createElement('h4');
+  h4.textContent = name;
+  let p = document.createElement('p');
+  h4.textContent = id;
+  let pokeType = document.createElement('p');
+  pokeType.textContent = type;
+  let img = document.createElement('p');
+  p.textContent = image;*/
+
+ /*let pokeName = document.createElement('h4');
  pokeName.textContent = name;
 
  let pokeId = document.createElement('h4');
@@ -43,6 +82,6 @@ function showPokemon ( id, name, type, image){
   mainContainer.appendChild(pokeName);
   mainContainer.appendChild(pokeId);
   mainContainer.appendChild(pokeType);
-  mainContainer.appendChild(pokeImage);
+  mainContainer.appendChild(pokeImage);*/
  
 }
