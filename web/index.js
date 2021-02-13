f61c5e195b8fee9040838b5723c81eb46f115a91
Lanay Harris <lanayharris@Lanays-Air.fios-router.home>
2021-02-13 12:49:08 -0500

    working on DOM

--- web/index.js
+++ web/index.js
@@ -24,19 +24,70 @@ button.addEventListener("click", () => {
 
  //function to create cards
 function showPokemon ( id, name, type, image){
-  let pokeName = document.createElement('h4');
- pokeName.textContent = name;
+  
+ let div = document.createElement('div');
+ div.className = 'container';
+ div.id = 'cardDeck'
+ div.innerHTML = 
+ '<div class="card">', '<div class="container">', '<div class="row">', '<h4 class="pName">Pikachu</h4>', '<h4 class="pNum">#025</h4>', '<div class="line"></div>', '</div>',
+   '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"></img>', 
+   '</div>', '</div>'
+ 
+ //'<h4.pName>name</h4>', '<h4.pNum>id</h4>', '<h4>type</h4>', '<div><img></img></div>' 
 
- let pokeId = document.createElement('h4');
+  let pokeName = document.createElement('h4.pName');
+ pokeName.textContent = name;
+ 
+
+ let pokeId = document.createElement('h4.pNum');
  pokeId.textContent = id;
+
 
  let pokeType = document.createElement('h4');
  pokeType.textContent = type;
+ 
 
  let pokeImage = document.createElement('sprites');
  pokeImage.textContent = image;
+
+
+
+ /*let div = document.createElement('div');
+ div.className = 'container';
+ div.id = 'cardDeck'
+
+ let div = document.createElement('div');
+ div.className = 'card';
+ div.innerHTML = '<p>CreateElement example</p>';
+
+ let div = document.createElement('div');
+ div.className = 'container';
+ div.innerHTML = '<p>CreateElement example</p>';
+
+ let div = document.createElement('div');
+ div.className = 'row';
+ div.innerHTML = '<h4>Pikachu</h4>';
+
+ let h = document.createElement('h4');
+ h.textContent = '025';
+ div.appendChild(h4);
  
-  return(
+ let div = document.createElement('div');
+ div.className = 'line';
+
+ let div = document.createElement('div');
+div.innerHTML = 'img';
+
+let img = document.createElement('img');
+img.innerHTML = image;
+div.appendChild(img);
+
+
+ 
+
+
+
+  /*return(
     <div class="container" id="cardDeck">
           <div class="card">
               <div class="container">
@@ -49,7 +100,7 @@ function showPokemon ( id, name, type, image){
               </div>
           </div>
       </div>
-  );
+  );*/
    //create a div for card follow html format
  //append to mainContainer.appendChild(variable);
  console.log(name, id, image, type);
@@ -76,12 +127,12 @@ function showPokemon ( id, name, type, image){
  pokeType.textContent = type;
 
  let pokeImage = document.createElement('sprites');
- pokeImage.textContent = image;
+ pokeImage.textContent = image;*/
  
  
   mainContainer.appendChild(pokeName);
   mainContainer.appendChild(pokeId);
   mainContainer.appendChild(pokeType);
-  mainContainer.appendChild(pokeImage);*/
+  mainContainer.appendChild(pokeImage);
  
 }
