504981b269417b260520aee252900df34b2db950
Lanay Harris <lanayharris@Lanays-MacBook-Air.local>
2021-02-14 07:47:25 -0500

    working on the DOM

--- web/index.js
+++ web/index.js
@@ -1,6 +1,27 @@
 const button = document.querySelector("button");
 const inputText = document.querySelector("#numberOfPokemons");
-const mainContainer = document.querySelector("#cardDeck");
+const mainContainer = document.querySelector("#poke_container");
+const colors = {
+bug: '#3b9950', 
+dark: '#5a5979',
+dragon: '#61cad9',
+electric: '#fbfb71',
+fairy: '#ea1169',
+fighting: '#ef6138',
+fire: '#fd4c59',
+flying: '#93b2c7',
+ghost: '#906790',
+grass: '#26cb4f',
+ground: '#6e491f', 
+ice: '#d9eefa',
+normal: '#c998a7',
+poison: '#9b69d8',
+psychic: '#f71c91',
+rock: '#8a3d22',
+steel: '#42bd94',
+water: '#86a9fa'
+};
+const main_types = Object.keys(colors);
 
 button.addEventListener("click", () => {
     let input = inputText.value;
@@ -25,97 +46,33 @@ button.addEventListener("click", () => {
  //function to create cards
 function showPokemon ( id, name, type, image){
   
- let div = document.createElement('div');
- div.className = 'container';
- div.id = 'cardDeck'
- div.innerHTML = 
- '<div class="card">', '<div class="container">', '<div class="row">', '<h4 class="pName">Pikachu</h4>', '<h4 class="pNum">#025</h4>', '<div class="line"></div>', '</div>',
-   '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"></img>', 
-   '</div>', '</div>'
+ const poke = document.createElement('div');
+ poke.classList.add('poke_container')
+ poke.className = 'container';
+ poke.id = 'cardDeck'
+
+ //const poke_types = pokemon[i]type.map(el => el.type.name) 
+ //const typeColor = main_types.find(type => poke_types.indexOf(type) > -1 );
+ const pokeInnerHTML = `
+  <div class="pokemon">
+    <div class="info">
+      <span class="pNum">${id}</span>
+      <h4 class="name">${name}</h4>
+      <h4 class="type"><span>${type}</span></h4>
+    </div>
+    <div class="img-container">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png"
+    </div>
+  </div>
  
- //'<h4.pName>name</h4>', '<h4.pNum>id</h4>', '<h4>type</h4>', '<div><img></img></div>' 
+ `;
 
-  let pokeName = document.createElement('h4.pName');
- pokeName.textContent = name;
- 
+ poke.innerHTML = pokeInnerHTML; 
 
- let pokeId = document.createElement('h4.pNum');
- pokeId.textContent = id;
-
-
- let pokeType = document.createElement('h4');
- pokeType.textContent = type;
- 
-
- let pokeImage = document.createElement('sprites');
- pokeImage.textContent = image;
-
-
-
- /*let div = document.createElement('div');
- div.className = 'container';
- div.id = 'cardDeck'
-
- let div = document.createElement('div');
- div.className = 'card';
- div.innerHTML = '<p>CreateElement example</p>';
-
- let div = document.createElement('div');
- div.className = 'container';
- div.innerHTML = '<p>CreateElement example</p>';
-
- let div = document.createElement('div');
- div.className = 'row';
- div.innerHTML = '<h4>Pikachu</h4>';
-
- let h = document.createElement('h4');
- h.textContent = '025';
- div.appendChild(h4);
- 
- let div = document.createElement('div');
- div.className = 'line';
-
- let div = document.createElement('div');
-div.innerHTML = 'img';
-
-let img = document.createElement('img');
-img.innerHTML = image;
-div.appendChild(img);
-
-
- 
-
-
-
-  /*return(
-    <div class="container" id="cardDeck">
-          <div class="card">
-              <div class="container">
-                <div class="row">
-                    <h4 class="pName">Pikachu</h4> 
-                    <h4 class="pNum">#025</h4>
-                    <div class="line"></div>
-                </div>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"></img>
-              </div>
-          </div>
-      </div>
-  );*/
    //create a div for card follow html format
  //append to mainContainer.appendChild(variable);
  console.log(name, id, image, type);
 
- /*let div = document.createElement('div');
- div.innerHTML = '<h4></h4>', '<p></p>', 
-
- let h4 = document.createElement('h4');
-  h4.textContent = name;
-  let p = document.createElement('p');
-  h4.textContent = id;
-  let pokeType = document.createElement('p');
-  pokeType.textContent = type;
-  let img = document.createElement('p');
-  p.textContent = image;*/
 
  /*let pokeName = document.createElement('h4');
  pokeName.textContent = name;
@@ -129,10 +86,10 @@ div.appendChild(img);
  let pokeImage = document.createElement('sprites');
  pokeImage.textContent = image;*/
  
- 
-  mainContainer.appendChild(pokeName);
+  mainContainer.appendChild(poke); 
+  /*mainContainer.appendChild(pokeName);
   mainContainer.appendChild(pokeId);
   mainContainer.appendChild(pokeType);
   mainContainer.appendChild(pokeImage);
- 
+ */
 }
