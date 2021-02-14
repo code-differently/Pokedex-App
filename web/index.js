c038997a0d68e8fa9ec18c509f67360cc3d56b59
Kim Lai <kimlai@Kims-MacBook-Air.local>
2021-02-14 14:00:47 -0500

    card

--- web/index.js
+++ web/index.js
@@ -1,5 +1,6 @@
 const button = document.querySelector("button");
 const inputText = document.querySelector("#numberOfPokemons");
+const container = document.querySelector(".container");
 
 button.addEventListener("click", () => {
     let input = inputText.value;
@@ -7,6 +8,27 @@ button.addEventListener("click", () => {
      .then((res) => res.json())
      .then((pokemon) => {
 
-       console.log(pokemon);
+      pokemon.map(i =>showPokemon(i.id, i.name, i.type, i.image));
+       
      });
  });
+
+
+function showPokemon(id, name, type, image){
+  
+  let div = document.createElement('div');
+  let divInnerHtml = 
+  `<div class="card">  
+    <div class="container">
+    <div class="row">
+      <h4 class="pName">${name}</h4>    
+      <h4 class="pNum">${id}</h4>
+      <div class="line"></div>
+    </div>
+    <img src="${image}";
+  </div>
+</div>`
+  div.innerHTML = divInnerHtml;
+  div.classList.add(type);
+  container.appendChild(div);
+}
