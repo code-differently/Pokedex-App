a787753eab742927389473ad583f69f6fcb542a9
Robert Woodruff <rwoodruff@Roberts-Air.fios-router.home>
2021-02-14 21:56:34 -0500

    need to push rest data to web to handle ajax call and populate cards on html page

--- rest/index.js
+++ rest/index.js
@@ -1,74 +1,26 @@
 // install nodemon to auto start server request instead of doing it manually. install with the followoing command: npm install -g nodemon
-// install pug - npm install pug --save
-// install middleware body parser : npm install body-parser --save
-// install middleware to read cookie data : npm install cookie-parser --save
+// install pug: npm install pug --save
+// install middleware body parser: npm install body-parser --save
+// install middleware to read cookie data: npm install cookie-parser --save
 
-
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
 const router = express.Router(); // <-- to use the router, replace all "app" with "router" : also, create a router folder and copy all route coding to the router folder
-const app = express()
-const bodyParser = require("body-parser");
-const port = 3000
 
-router.get('/handle',(req,resp) => {
-  //code to perform particular action.
-  //To access GET variable use req.query() and req.params() methods.
-});
+const app = express();
+const port = 3000;
 
-// add router in the Express app.
-app.use("/", router);
+//app.use(bodyParser.urlencoded( { extended: false }));
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Test3!');
+app.get('/pokemon/:id', async (req, res) => {
+  let pokemonId = req.params.id
+  let pokedexResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+  res.send(pokedexResponse);
 })
-
-app.get('/pokemon', async (req, res) => {
-    let pokemonResponse = await axios.get("https://pokeapi.co/api/v2/pokemon/")
-    res.send(pokemonResponse)
-    console.log(pokemonResponse);
-
-// reference code for express API call ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const https = require('https');
-
-https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
-  let data = '';
-
-  // A chunk of data has been received.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data).explanation);
-  });
-
-}).on("error", (err) => {
-  console.log
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 app.listen(port, () => {
-  console.log(`Example app listening at n`)
+  console.log(`Test to show that app is listening on http://localhost:${port}`)
 })
-
-
-
-// example code for dog api lab ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// const express = require('express')
-// const axios = require('axios');
-// const cors = require("cors")
-
-// const app = express()
-// const port = 4000
-
-// app.use(cors())
-
-// app.get('/dog/random', async (req, res) => {
-//   let dogResponse = await axios.get("https://dog.ceo/api/breeds/image/random")
-//   res.send(dogResponse.data.message)
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- web/index.js
+++ web/index.js
@@ -1,54 +1,61 @@
 let id;
 let img;
-let energyType = [ 
-    'grass', 
-    'lightning', 
-    'darkness', 
-    'fairy', 
-    'fire', 
-    'psychic', 
-    'metal', 
-    'dragon', 
-    'water', 
-    'fighting', 
-    'colorless'
-];
-
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-        let rolodex = JSON.parse(xhr.responseText);
-        let pokemon = document.getElementById('pokemon');
-        const name = rolodex.results[0].name.toUpperCase();
-        pokemon.innerHTML = name;
-        console.log(rolodex);
-    }
-}
-    xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/');
-    xhr.send();
+// let energyType = [ 
+//     'grass', 
+//     'lightning', 
+//     'darkness', 
+//     'fairy', 
+//     'fire', 
+//     'psychic', 
+//     'metal', 
+//     'dragon', 
+//     'water', 
+//     'fighting', 
+//     'colorless'
+// ];
 
 let details = new XMLHttpRequest();
 details.onreadystatechange = function() {
-    if (details.readyState === 4) {
-        let abilities = JSON.parse(details.responseText);
-        console.log(abilities);
+    if (this.readyState === 4) {
+        document.getElementById('cardGrid').innerHTML = this.responseText;
+        //console.log(pokemonData);
+        // let charInfo = document.getElementById('charInfo');
+        // charInfo.innerHTML = this.responseText;
     }
 }
+    //details.open('GET', 'http://localhost:3000/pokemon/id');
     details.open('GET', 'https://pokeapi.co/api/v2/pokemon/1');
     details.send();
 
-// example code from dog api ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function getRandomDoggiePic(){
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200) {
-            document.getElementById("doggie").src = this.responseText;
-        }
-    }
+//function getPokemonData() {
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+//     if (xhr.readyState === 4) {
+//         let rolodex = JSON.parse(xhr.responseText);
+//         //const name = rolodex.results[0].name.toUpperCase();
+//         //let pokemon = document.getElementById('pokemon');
+//         //pokemon.innerHTML = this.responseText;
+//         console.log(rolodex);
+//     }
+// }
+//     //xhr.open('GET', 'http://localhost:3000/pokemon');
+//     xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/');
+//     xhr.send();
 
-    xhr.open("GET", "http://localhost:4000/dog/random")
-    xhr.send();
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-module.exports = router; // <-- this how you reference an external file, router is the declared argument in another file
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ example code from dog api ~~~~~~~~~~~
+
+// function getRandomDoggiePic(){
+//     let xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function() {
+//         if(this.readyState === 4 && this.status === 200) {
+//             document.getElementById("doggie").src = this.responseText;
+//         }
+//     }
+
+//     xhr.open("GET", "http://localhost:4000/dog/random")
+//     xhr.send();
+// }
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//module.exports = router; // <-- this how you reference an external file, router is the declared argument in another file
